81a57defe930fa0599204848447c4ac0b1f44965
Lucas Rebello <lucas.rebello@alura.com.br>
2025-04-18 11:20:35 -0300

    spider-man

---
+++ lua/plugins/others.lua
@@ -0,0 +1,75 @@
+return {
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+	-- Highlight todo, notes, etc in comments
+	{
+		-- PERF:
+		-- TEST: If you have to test smth, text will be white
+		-- TODO: for todo on code
+		-- NOTE: for notes on code
+		-- WARN: for warnings on code
+		-- WARNING:
+		-- HACK:
+		-- FIXME:
+		-- FIX:
+		-- BUG:
+		-- FIXIT:
+		-- ISSUE: If you need to fix smth, the comment will be red
+
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+	{ -- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = "ibl",
+		opts = {},
+		config = function()
+			require("ibl").setup({
+				indent = { char = "▏" },
+				scope = { show_start = false, show_end = false },
+			})
+		end,
+	},
+
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			-- Better Around/Inside textobjects
+			--
+			-- Examples:
+			--  - va)  - [V]isually select [A]round [)]paren
+			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+			--  - ci'  - [C]hange [I]nside [']quote
+			require("mini.ai").setup({ n_lines = 500 })
+
+			-- Add/delete/replace surroundings (brackets, quotes, etc.)
+			--
+			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+			-- - sd'   - [S]urround [D]elete [']quotes
+			-- - sr)'  - [S]urround [R]eplace [)] [']
+			require("mini.surround").setup()
+
+			-- Simple and easy statusline.
+			--  You could remove this setup call if you don't like it,
+			--  and try some other statusline plugin
+			local statusline = require("mini.statusline")
+			-- set use_icons to true if you have a Nerd Font
+			statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+			-- You can configure sections in the statusline by overriding their
+			-- default behavior. For example, here we set the section for
+			-- cursor location to LINE:COLUMN
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
+
+			-- ... and there is more!
+			--  Check out: https://github.com/echasnovski/mini.nvim
+		end,
+	},
+}
